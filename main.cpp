cac514e23219a2e3382c1d58a73a00bfa5ecedad
Hudson Bean <hudsonbeandev@gmail.com>
2026-05-27 09:47:18 -0500

    Made a basic triangle wireframe with colors.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,33 @@
 #include "tgaimage.h"
 #include <cmath>
+#include <iostream>
 
 constexpr TGAColor white = {255, 255, 255, 255}; // attention, BGRA order
 constexpr TGAColor green = {0, 255, 0, 255};
 constexpr TGAColor red = {0, 0, 255, 255};
 constexpr TGAColor blue = {255, 128, 64, 255};
 constexpr TGAColor yellow = {0, 200, 255, 255};
+
+void drawLine(int ax, int ay, int bx, int by, TGAImage &framebuffer,
+              TGAColor color) {
+  bool steep = std::abs(ax - bx) < std::abs(ay - by);
+  if (steep) { // if the line is steep, we transpose the image
+    std::swap(ax, ay);
+    std::swap(bx, by);
+  }
+  if (ax > bx) { // make it left−to−right
+    std::swap(ax, bx);
+    std::swap(ay, by);
+  }
+  for (int x = ax; x <= bx; x++) {
+    float t = (x - ax) / static_cast<float>(bx - ax);
+    int y = std::round(ay + (by - ay) * t);
+    if (steep) // if transposed, de−transpose
+      framebuffer.set(y, x, color);
+    else
+      framebuffer.set(x, y, color);
+  }
+}
 
 int main(int argc, char **argv) {
   constexpr int width = 64;
@@ -16,9 +38,9 @@ int main(int argc, char **argv) {
   int bx = 12, by = 37;
   int cx = 62, cy = 53;
 
-  framebuffer.set(ax, ay, white);
-  framebuffer.set(bx, by, white);
-  framebuffer.set(cx, cy, white);
+  drawLine(ax, ay, bx, by, framebuffer, blue);
+  drawLine(cx, cy, bx, by, framebuffer, green);
+  drawLine(ax, ay, cx, cy, framebuffer, red);
 
   framebuffer.write_tga_file("framebuffer.tga");
   return 0;
